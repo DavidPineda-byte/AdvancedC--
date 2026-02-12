b3f205d1d0cf8da041ff427a0b51f9b53f24df90
david <edavidpinedam@gmail.com>
2026-02-12 14:45:37 -0500

    Add C++ project configuration and implement student management features
    - Create C++ configuration for VSCode.
    - Update build tasks to use C++23 standard.
    - Refactor abstractManager class to use std namespace and improve code formatting.
    - Implement display method in studentManager class for formatted output.
    - Add test file to demonstrate student management functionality.

--- abstractManager.h
+++ abstractManager.h
@@ -1,12 +1,12 @@
 #include "student.h"
 #include <vector>
+#include <string>
 
 class abstractManager{
-    public: 
-        virtual void add(Student student)=0;
-        virtual void display()=0;
-        virtual Student search(int id)=0; 
-        virtual vector<Student> searchByName(string name)=0;
-        virtual ~abstractManager() = default;
-
+public: 
+    virtual void add(Student student)=0;
+    virtual void display()=0;
+    virtual Student search(int id)=0; 
+    virtual std::vector<Student> searchByName(std::string name)=0;
+    virtual ~abstractManager() = default;
 };
---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <format>
+#include <iostream>
+#include "studentManager.h"
+
+
+int main() {
+
+studentManager manager;
+
+Student student1("Alice", 20, "Computer Science", "alice@bhcc.edu");
+Student student2("David", 24, "CS", "david@bhcc.edu");
+
+manager.add(student1);
+manager.add(student2);
+
+manager.display();
+
+}
