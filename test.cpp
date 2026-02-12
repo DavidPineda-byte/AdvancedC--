4a7f381344a69f59ca78465ddc706c11d86d6290
david <edavidpinedam@gmail.com>
2026-02-12 18:17:37 -0500

    added one more student to test file

--- test.cpp
+++ test.cpp
@@ -9,9 +9,11 @@ studentManager manager;
 
 Student student1("Alice", 20, "Computer Science", "alice@bhcc.edu");
 Student student2("David", 24, "English", "david@bhcc.edu");
+Student student3("Teresa", 20, "Computer Science", "teresa@umassb.edu");
 
 manager.add(student1);
 manager.add(student2);
+manager.add(student3);
 
 manager.display();
 manager.studentsByMajor();
